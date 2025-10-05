543027495283539ee38f6f03e0af16bcdc7b1729
tanuinfaith <tanuinfaith@gmail.com>
2025-10-05 17:13:05 +0530

    Add files via upload

---
+++ python task 1.py
@@ -0,0 +1,12 @@
+no1 = int(input("enter a number!"))
+no2 = int(input("enter another number!"))
+print("now,we'll perform all the operations on them")
+
+add = no1 + no2
+print(f" Addition: {no1}+{no2} is {add}")
+sub = no1 - no2
+print(f" Subtraction: {no1}-{no2} is {sub}")
+mul = no1 * no2
+print(f" Multiplication: {no1}*{no2} is {mul}")
+div = no1 / no2
+print(f" Division: {no1}/{no2} is {div}")
---
+++ python task 2.py
@@ -0,0 +1,4 @@
+fname= input("Hey, what's your first name?")
+lname = input("What's your last name?")
+name = (f"{fname}"+" " + f"{lname}")
+print(f"Wow! You have a truly beautiful name, {name}.It's so nice to meet you!")
